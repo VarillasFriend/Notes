160a8d7a679588d54ce75c24e4446bb0ea09e233
Juan Andres Olmedo <olmedo.juanandres5@gmail.com>
2021-05-07 13:29:52 -0300

    Fixed an issue with buttons on iOS

--- index.js
+++ index.js
@@ -538,3 +538,23 @@ function setPassword() {
         passwordForm2.classList.remove("show");
     };
 }
+
+// function download(data, filename, type) {
+//     var file = new Blob([data], {type: type});
+//     if (window.navigator.msSaveOrOpenBlob) // IE10+
+//         window.navigator.msSaveOrOpenBlob(file, filename);
+//     else { // Others
+//         var a = document.createElement("a"),
+//                 url = URL.createObjectURL(file);
+//         a.href = url;
+//         a.download = filename;
+//         document.body.appendChild(a);
+//         a.click();
+//         setTimeout(function() {
+//             document.body.removeChild(a);
+//             window.URL.revokeObjectURL(url);
+//         }, 0);
+//     }
+// }
+
+// download(Note.all()[0].body, Note.all()[0].title, "text/markdown")
